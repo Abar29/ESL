23cceb6ae39f2e4fc34ffa8c44862215b22b52dc
Abar29 <abarientosernesto29@gmail.com>
2026-09-02 20:03:14 +0800

    feat: add My Profile to student sidebar (opens Settings modal)

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -28,8 +28,10 @@ const openProfileModal = async () => {
     sidebarOpen.value = false;
     if (userRole.value === 'teacher') {
         await fetchTeacherProfile();
+        showProfileModal.value = true;
+    } else {
+        showSettingsModal.value = true;
     }
-    showProfileModal.value = true;
 };
 
 const fetchTeacherProfile = async () => {
@@ -63,6 +65,7 @@ const navigation = computed(() => {
         items.push(
             { name: 'Browse Teachers', href: '/student/teachers', icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z' },
             { name: 'My Bookings', href: '/student/bookings', icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' },
+            { name: 'My Profile', action: 'profile', icon: 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z' },
         );
     } else if (role === 'teacher') {
         items.push(
